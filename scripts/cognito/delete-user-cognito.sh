2fa22d76b0a7461888478010f974b76c65774e45
Andres Molano <69824011+amolanol5@users.noreply.github.com>
2024-11-02 10:41:10 -0500

    test

--- scripts/cognito/delete-user-cognito.sh
+++ scripts/cognito/delete-user-cognito.sh
@@ -4,27 +4,23 @@
 echo "installing yq and microcks-cli"
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
 
+# Archivo YAML
+cd $FOLDER_COGNITO_USERS
+YAML_FILE="users-dev.yaml"
+
+
 # delete cognito user
-echo "deleting user"
-echo $FOLDER_COGNITO_USERS
-echo "aws cognito-idp admin-delete-user --user-pool-id $POOL_ID --username $USERNAME"
-
-
-
-
-
-
-
-# # finding files
-# path="service-mocks/api-mocks"
-# yaml_files=$(find  $path/*/ -type f -maxdepth 1  -name "*.yml")
-
-# # import mocks
-# echo "importing mocks to microcks"
-# for files in $yaml_files; do
-#     echo " ******** importing: $files ******** " 
-#     microcks-cli import "$files" \
-#         --microcksURL=$MICROCKS_BASE_URL/api/ \
-#         --keycloakClientId=$MICROCKS_CLIENT_ID \
-#         --keycloakClientSecret=$MICROCKS_CLIENT_SECRET
-# done
+# Iterar sobre los pools
+yq '.pools[]' "$YAML_FILE" | while IFS= read -r pool; do
+  # Extraer el id del pool
+  POOL_ID=$(echo "$pool" | yq '.id')
+  
+  # Iterar sobre los usuarios dentro del pool
+  echo "$pool" | yq '.users_to_delete[]' | while IFS= read -r user; do
+    # Extraer el email (usado como nombre de usuario en este caso)
+    USERNAME=$(echo "$user" | yq '.email')
+    
+    # Ejecutar el comando AWS
+    echo "aws cognito-idp admin-delete-user --user-pool-id $POOL_ID --username $USERNAME "
+  done
+done
