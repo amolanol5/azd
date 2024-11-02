19330f8e42b945e0ceca90f30b46b1a9ff50670d
Andres Molano <69824011+amolanol5@users.noreply.github.com>
2024-11-02 13:45:26 -0500

    test

--- scripts/cognito/delete-user-cognito.sh
+++ scripts/cognito/delete-user-cognito.sh
@@ -21,17 +21,19 @@ jq -c '.pools[]' "$JSON_FILE" | while read -r pool; do
   jq -c '.users_to_delete[]' <<< "$pool" | while read -r user; do
     USERNAME=$(jq -r '.id' <<< "$user")
 
-    # Verificar si el usuario existe en el pool
-    aws cognito-idp admin-get-user --user-pool-id "$POOL_ID" --username "$USERNAME" &>/dev/null
+    echo "aws cognito-idp admin-delete-user --user-pool-id $POOL_ID --username $USERNAME"
 
-    if [ $? -eq 0 ]; then
-      # Si el usuario existe, eliminarlo
-      echo "aws cognito-idp admin-delete-user --user-pool-id "$POOL_ID" --username "$USERNAME""
-      echo "Usuario $USERNAME eliminado del pool $POOL_ID"
-    else
-      # Si el usuario no existe, registrar en el log
-      echo "Usuario $USERNAME no existe o ya fue eliminado del pool $POOL_ID"
-    fi
+    # Verificar si el usuario existe en el pool
+    # aws cognito-idp admin-get-user --user-pool-id "$POOL_ID" --username "$USERNAME" &>/dev/null
+
+    # if [ $? -eq 0 ]; then
+    #         # Si el usuario existe, eliminarlo
+    #         echo "aws cognito-idp admin-delete-user --user-pool-id "$POOL_ID" --username "$USERNAME""
+    #         echo "Usuario $USERNAME eliminado del pool $POOL_ID"
+    # else
+    #         # Si el usuario no existe, registrar en el log
+    #         echo "Usuario $USERNAME no existe o ya fue eliminado del pool $POOL_ID"
+    # fi
 
   done
 done
